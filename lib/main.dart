29cb491d4bb4c1abffda440968f387ef7c6ab584
Fc4 <netcyberjo@gmail.com>
2026-02-27 09:07:43 -0800

    a3

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+// ایمپورت کردن تمام صفحات
 import 'features/auth/login_screen.dart';
-import 'features/main_layout/main_screen.dart'; // اضافه شدن فایل جدید
+import 'features/main_layout/main_screen.dart';
+import 'features/memories/memories_screen.dart';
+import 'features/memories/add_memory_screen.dart';
 import 'features/memories/memory_detail_screen.dart';
-import 'features/profile/profile_screen.dart';
-import 'features/daily/daily_screen.dart'; // NEW: مسیر فایل جدید
-import 'features/profile/edit_profile_screen.dart'; // NEW
+import 'features/daily/daily_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,13 +41,14 @@ class DelbarApp extends StatelessWidget {
         textTheme: GoogleFonts.vazirmatnTextTheme(Theme.of(context).textTheme),
       ),
       initialRoute: initialRoute,
-	  getPages: [
+      getPages: [
         GetPage(name: '/login', page: () => LoginScreen()),
         GetPage(name: '/main', page: () => MainScreen()),
         GetPage(name: '/memories', page: () => MemoriesScreen()),
         GetPage(name: '/add_memory', page: () => AddMemoryScreen()),
-        GetPage(name: '/memory_detail', page: () => const MemoryDetailScreen()), // <--- NEW
-		GetPage(name: '/edit_profile', page: () => EditProfileScreen()),
+        // کلمه const از خط زیر حذف شد تا ارور برطرف شود
+        GetPage(name: '/memory_detail', page: () => MemoryDetailScreen()),
+        GetPage(name: '/daily', page: () => DailyScreen()),
       ],
     );
   }
